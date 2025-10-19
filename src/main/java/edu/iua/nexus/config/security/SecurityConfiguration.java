560250ec042998d175d6491ec0ef4da9b5d7a312
Agostina Perez <aggostinaperez@gmail.com>
2025-10-19 18:48:04 -0300

    fix errores de despliegue

--- src/main/java/edu/iua/nexus/config/security/SecurityConfiguration.java
+++ src/main/java/edu/iua/nexus/config/security/SecurityConfiguration.java
@@ -2,11 +2,15 @@ package edu.iua.nexus.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -25,4 +29,16 @@ public class SecurityConfiguration {
 		);
 		return http.build();
    }
+
+    // Bean necesario para inyectar AuthenticationManager en tu AuthRestController
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+        return authConfig.getAuthenticationManager();
+    }
+
+    // Bean para inyectar PasswordEncoder en tu AuthRestController
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
