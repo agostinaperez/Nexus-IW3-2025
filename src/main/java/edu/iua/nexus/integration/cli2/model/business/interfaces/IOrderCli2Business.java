d653bf26fbee0d9431d305cac39a0c8ca9b7a879
Matias Quiroga <mquiroga017@alumnos.iua.edu.ar>
2025-11-01 18:02:28 -0300

    metodo privado, ENDPOINT 5.1
    Metodo privado para reutilizacion de codigo y "nuevo" endpoint agregando la posibilidad de la conciliacion en cualquier momento

--- src/main/java/edu/iua/nexus/integration/cli2/model/business/interfaces/IOrderCli2Business.java
+++ src/main/java/edu/iua/nexus/integration/cli2/model/business/interfaces/IOrderCli2Business.java
@@ -11,6 +11,9 @@ public interface IOrderCli2Business {
 
     Order registerInitialWeighing(String orderNumber, float initialWeight) throws BusinessException, NotFoundException, FoundException, ConflictException;
 
-    //ENDPOINT 5
+    //ENDPOINT 5.0
     byte[] registerFinalWeighing(String orderNumber, float finalWeight) throws BusinessException, NotFoundException, FoundException, ConflictException;
+
+    //ENDPOINT 5.1
+    byte[] getConciliation(String licensePlate) throws BusinessException, NotFoundException;
 }
